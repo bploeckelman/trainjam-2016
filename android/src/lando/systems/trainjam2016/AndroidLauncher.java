635c390e0e5d1830842d39d9fd3f553fe5018c64
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-03-10 15:50:20 -0600

    Inital Commit

---
+++ android/src/lando/systems/trainjam2016/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package lando.systems.trainjam2016;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lando.systems.trainjam2016.TrainJam2016;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TrainJam2016(), config);
+	}
+}
---
+++ desktop/src/lando/systems/trainjam2016/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.trainjam2016.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.trainjam2016.TrainJam2016;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TrainJam2016(), config);
+	}
+}
