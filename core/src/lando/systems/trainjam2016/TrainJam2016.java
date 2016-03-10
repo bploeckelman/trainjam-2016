d70ab11f724fb8187016a89ae9de81a0e19064ba
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-03-10 16:43:00 -0600

    Add tween engine dependencies and some placeholder stuff

--- core/src/lando/systems/trainjam2016/TrainJam2016.java
+++ core/src/lando/systems/trainjam2016/TrainJam2016.java
@@ -5,23 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import lando.systems.trainjam2016.utils.Assets;
 
 public class TrainJam2016 extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Assets.load();
+		batch = Assets.batch;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(Assets.testTexture, 0, 0);
 		batch.end();
 	}
 }
