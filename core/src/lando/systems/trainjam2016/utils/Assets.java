e1514d6e468f5e0a1d90927b960ba72b0df0d123
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-03-11 15:09:56 -0600

    Switch bags, show bag capacity

--- core/src/lando/systems/trainjam2016/utils/Assets.java
+++ core/src/lando/systems/trainjam2016/utils/Assets.java
@@ -34,6 +34,7 @@ public class Assets {
     public static Texture gridTexture;
 
     public static Texture bagTexture;
+    public static Texture bagSmallTexture;
     public static Texture appleTexture;
     public static Texture soupTexture;
 
@@ -69,6 +70,7 @@ public class Assets {
         grid = new TextureRegion(gridTexture);
 
         bagTexture = new Texture("bag.png");
+        bagSmallTexture = new Texture("bag-small.png");
         appleTexture = new Texture("apple.png");
         soupTexture = new Texture("soup.png");
         bag = new TextureRegion(bagTexture);
@@ -83,6 +85,7 @@ public class Assets {
         whitePixelTexture.dispose();
         whiteCircleTexture.dispose();
         bagTexture.dispose();
+        bagSmallTexture.dispose();
         appleTexture.dispose();
         gridTexture.dispose();
         soupTexture.dispose();
