ac512fb8e5ede5c0859e47a52d9d5cae0f295c97
Michael Tolly <miketolly@gmail.com>
2016-03-12 13:39:45 -0800

    Add a new item every few seconds

--- core/src/lando/systems/trainjam2016/screens/GameScreen.java
+++ core/src/lando/systems/trainjam2016/screens/GameScreen.java
@@ -37,6 +37,7 @@ public class GameScreen extends BaseScreen {
     Vector2          dragDist;
     Color            capacityColor;
     float            currentTime;
+    float            timeOfNextItem;
 
     public GameScreen() {
         super();
@@ -61,9 +62,8 @@ public class GameScreen extends BaseScreen {
         doneBags = new Array<Bag>();
 
         items = new Array<Item>();
-        Item bowl = new ItemBowl();
-        bowl.setConveyorTime(2);
-        items.add(bowl);
+        currentTime = 0;
+        timeOfNextItem = 0;
 
         conveyor = new Conveyor();
 
@@ -123,6 +123,14 @@ public class GameScreen extends BaseScreen {
         }
         activeBag.update(dt);
         conveyor.update(dt);
+
+        if (timeOfNextItem <= currentTime) {
+            Item newItem = Item.createNewRandomItem();
+            newItem.setConveyorTime(currentTime + 1);
+            newItem.moveTo(-200, -200);
+            items.add(newItem);
+            timeOfNextItem = currentTime + 4;
+        }
     }
 
     @Override
@@ -245,11 +253,6 @@ public class GameScreen extends BaseScreen {
                         Assets.place.play(Const.volume * 2f);
                         selectedItem.moveToCell();
                         items.removeValue(selectedItem, true);
-
-                        Item newItem = Item.createNewRandomItem();
-                        newItem.setConveyorTime(currentTime);
-                        newItem.moveToCell(MathUtils.random(1, 10), MathUtils.random(1, 10));
-                        items.add(newItem);
                     } else {
                         Assets.drop.play(Const.volume * 2f);
                         selectedItem.moveToCell(originalCellX, originalCellY);
