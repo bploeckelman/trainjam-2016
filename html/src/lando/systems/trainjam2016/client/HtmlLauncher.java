a5c5857807d9de70ffefae72a3822f1368e633eb
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-03-12 11:23:56 -0600

    Fix web build

--- html/src/lando/systems/trainjam2016/client/HtmlLauncher.java
+++ html/src/lando/systems/trainjam2016/client/HtmlLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import lando.systems.trainjam2016.TrainJam2016;
+import lando.systems.trainjam2016.utils.Const;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Const.width, Const.height);
         }
 
         @Override
